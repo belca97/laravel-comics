2c72e451b5e61717a07fb4ca9eaf11382e40c05f
Matteo Belcari <matteobelcari977@gmail.com>
2022-03-29 17:45:52 +0200

    Prima Parte es finita

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
+@extends('layouts.base')
 
-    @include('partials.header')
-    @include('partials.jumbotron')
+@section('pageTitle', 'home')
+    
+@section('content')
+<div class="container">
 
-    <main>
+    @foreach ($fumetti as $fumetto )
+            <div class="box-img">
+                <img src="{{$fumetto['thumb']}}" :alt="{{$fumetto['thumb']}}"">
+                <h3>{{$fumetto['series']}}</h3> 
+            </div>
+        
+    @endforeach
 
-    </main>
-
-    @include('partials.footer-first')
-    @include('partials.footer-second')
-    @include('partials.footer-third')
-
-</body>
-
-<style>
-    *{
-        margin: 0 auto;
-        padding: 0;
-        box-sizing: border-box;
-    }
-</style>
-</html>
+    <div class="container-button">
+        <button class="button-main">LOAD MORE</button>
+    </div>
+</div>
+@endsection
